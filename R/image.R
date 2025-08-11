7236203618e3e6103a6e072f1b2303b1cd7c7d76
mike <mikefc@coolbutuseless.com>
2025-08-11 20:51:09 +1000

    factoring out image conversion

---
+++ R/image.R
@@ -0,0 +1,76 @@
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#' Convert image to bytes for RGB + Alpha (or Gray + Alpha)
+#' 
+#' @param im image. numeric values in range [0, 255]
+#' \itemize{
+#'   \item{Grayscale - numeric matrix}
+#'   \item{Grayscale with alpha - numeric array with 2 planes}
+#'   \item{RGB - numeric array with 3 planes}
+#'   \item{RGB with alpha - numeric array with 4 planes}
+#'   \item{RGB with alpha - nativeraster}
+#' }
+#' @return named list of 3 elements: type, rgb (or g) and alpha
+#' @export
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+image_to_bytes <- function(im) {
+
+  res        <- list()
+  res$width  <- ncol(im)
+  res$height <- nrow(im)
+  
+  if (is.matrix(im)) {
+    res$type <- 'G' 
+    
+    res$pixels <- t(im)
+    res$alpha  <- rep(255, res$width * res$height) # Fake alpha
+    
+  } else if (is.array(im)) {
+    stopifnot(length(dim(im)) == 3)
+    nplanes <- dim(im)[3]
+    
+    if (nplanes == 2) {
+      stop("image_to_bytes(): 2 planes not done")
+      res$type == 'G'
+    } else if (nplanes == 3) {
+      stop("image_to_bytes(): 3 planes not done")
+      res$type == 'RGB'
+    } else if (nplanes == 4) {
+      stop("image_to_bytes(): 4 planes not done")
+      res$type == 'RGB'
+    } else {
+      stop("Unhandled num of planes: ", nplanes)
+    }
+    
+  } else if (inherits(im, 'nativeRaster')) {
+    res <- nr_to_bytes(im)
+  }
+  
+  res
+}
+
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#' NativeRaster to bytes
+#' 
+#' @param nr nativeRaster
+#' @return named list of 3 elements: type, rgb (or g) and alpha
+#' @noRd
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+nr_to_bytes <- function(nr) {
+  stop("nr_to_bytes(): not done yet")
+  
+  res <- list()
+  res$type   <- 'RGBA'
+  res$width  <- ncol(nr)
+  res$height <- nrow(nr)
+  
+  res$pixels <- NULL
+  res$alpha  <- NULL
+  
+  res
+}
+
+
